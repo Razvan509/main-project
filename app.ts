a6e5c97357c6ec1627ca06c1f519cde92c9a73f0
Razvan509 <razvanrang@gmail.com>
2021-01-11 17:39:40 +0200

    commit

--- app.ts
+++ app.ts
@@ -1,15 +1,33 @@
-function add (n1: number, n2: number, showResult: boolean){
-    if (showResult){
-        console.log(typeof number1);
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+
+function combine(
+    input1: Combinable, 
+    input2: Combinable, 
+    resultConversion: ConversionDescriptor
+){
+
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2;
     }else{
-        return n1 + n2;    
+        result = input1.toString() + input2.toString();
     }
+    return result;
+    // if (resultConversion === 'as-number'){
+    //     return +result;
+    // }else{
+    //     return result.toString();
+    // }
     
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
 
-const result = add(number1,number2, printResult);
-console.log(result);
+const combinedNames = combine('Raz' , 'Teo', 'as-text');
+console.log(combinedNames);
+
+const combinedStringAges = combine('30' , '26', 'as-number');
+console.log(combinedStringAges);
---
+++ things.ts
@@ -0,0 +1,47 @@
+// const person = {
+//     name: string;
+//     age: number;
+// } 
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Razvan',
+//     age: 22,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+};
+
+const person = {
+    name: 'Razvan',
+    age: 22,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+
+//person.role.push('admin');
+//person.role[0] = 10;
+
+
+//person.role = [0, 'admin'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase()); 
+}
+
+if (person.role === Role.ADMIN){
+    console.log('is admin');
+}
